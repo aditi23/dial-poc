d223ba5cebee092da2e0b2c6f927d314ba4e5183
Aditi Singh <aditi.singh@tokopedia.com>
2021-07-02 20:08:33 +0530

    use cgo

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/aditi23/dial-poc/handlers"
 )
 
+func init() {
+	os.Setenv("GODEBUG", "netdns=1")
+}
+
 func main() {
+	fmt.Println("GODEBUG", os.Getenv("GODEBUG"))
 	log.Println("Server started on: http://localhost:8998")
 	http.HandleFunc("/tcp4", handlers.TCP4)
 	http.HandleFunc("/tcp", handlers.TCP)
